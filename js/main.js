04ff7a4b769bb9bf86ba302efaa7735fbdde6533
Dipesh sapkota <sapkotadipes@gmail.com>
2023-10-02 08:33:02 +0545

    changed folder

---
+++ js/main.js
@@ -0,0 +1,12 @@
+ 
+  const Email_Input = document.getElementById("email-input");
+  
+  function Fill_Placeholder(){
+    Email_Input.value = 'john#mail.com';
+    
+  } 
+   function Empty_Placeholder(){
+    Email_Input.value = '';
+    
+   }
+ 
